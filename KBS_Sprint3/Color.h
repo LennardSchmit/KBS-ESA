c312f396b382b4ae8acf21e4bebe821e0118aed1
LennardSchmit <lennard.98@live.nl>
2016-12-22 16:11:25 +0100

    Kleuren toegevoegd

--- KBS_Sprint3/Color.h
+++ KBS_Sprint3/Color.h
@@ -2,7 +2,7 @@
 #define COLOR_H_
 
 //Colors for drawing players
-#define PLAYERBLACK   RGB(32, 32, 32)     //1
+#define PLAYERBLACK   RGB(32, 32, 32)     //1     3
 #define GREEN1  RGB(96, 208, 80)    //2
 #define GREEN2  RGB(64, 152, 48)    //3
 #define GREEN3  RGB(32, 112, 16)    //4
@@ -15,13 +15,16 @@
 #define WHITE2  RGB(220, 220, 220)  //11
 #define BLUE1   RGB(128, 160, 192)  //12
 #define BLUE2   RGB(64, 96, 128)    //13
-#define RED1    RGB(232, 56, 64)
-#define RED2    RGB(176, 16, 24)
-#define RED3    RGB(128, 0, 0)
+#define RED1    RGB(232, 56, 64)    //14 
+#define RED2    RGB(176, 16, 24)    //15
+#define RED3    RGB(128, 0, 0)      //16
+#define GREY1   RGB(171, 171, 181)  //17
+#define GREY2   RGB(113, 118, 122)  //18
+#define ORANGE1  RGB(255, 127, 39)
 
 //Colors for GameField and Menu's
 #define RED   RGB(194, 91, 86)    //this is the color for the block that are destroyable
-#define BACKGROUND    RGB( 116, 130, 143)      //this is the color for the blocks on the outside as well as the blocks that are undestroyable in the game
-#define BLACK   RGB(52, 54, 65)       //this is the color for the field you are standing on
+#define BACKGROUND    RGB( 116, 130, 143)      //1  this is the color for the blocks on the outside as well as the blocks that are undestroyable in the game
+#define BLACK   RGB(52, 54, 65)       //2 this is the color for the field you are standing on
 
 #endif /* COLOR_H_ */
